4ab927fcce3a2362949e1e59811a7fd89fe67837
marvpaul <marvin.krueger@yahoo.de>
2022-01-30 12:30:46 +0100

    Public variables

---
+++ Assets/Keyboard.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyboard : MonoBehaviour
+{
+    public GameObject blackTile, whiteTile; 
+    public GameObject content;
+
+    public int numberOfOctaves;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/PianoTile.cs
+++ Assets/PianoTile.cs
@@ -5,4 +5,5 @@ using UnityEngine;
 public class PianoTile : MonoBehaviour
 {
     public int midiNote;
+
 }
